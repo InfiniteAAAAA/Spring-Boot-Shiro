7cbaae8afc52e1131350cb54d8a2611b1f961225
xwj <394225235@qq.com>
2019-05-10 17:33:37 +0800

    初始化Spring Boot Shiro项目

---
+++ src/main/java/com/infinite/pojo/User.java
@@ -0,0 +1,17 @@
+package com.infinite.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class User implements Serializable {
+    private static final long serialVersionUID = -5633278569387528804L;
+    private Integer id;
+    private String userName;
+    private String password;
+    private Date createTime;
+    private String status;
+
+}
